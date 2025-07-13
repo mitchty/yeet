e3572a961a36bc5eb99fb60ee79b40dcd8919f29
Mitchell Tishmack <git@mitchty.net>
2025-07-13 11:14:58 -0500

    chore: add formatters and fenix for latest rust stable
    Need to treat this kosher for future efforts

--- flake.nix
+++ flake.nix
@@ -2,14 +2,19 @@
   description = "WIP DO NOT USE THIS IS A TOTAL HOLD MY BEER EXPERIMENT";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     crane.url = "github:ipetkov/crane";
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -19,19 +24,41 @@
   #
   # What I want to test is linux specific anyway. When I get a new macbook air I
   # can put in the effort to port things.
-  outputs = { nixpkgs, crane, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (
+      system:
       let
-        pkgs = import nixpkgs {
+        pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
         };
 
+        treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            rustfmt.enable = true;
+            taplo.enable = true;
+          };
+        };
         inherit (pkgs) lib;
 
-        craneLib = if system == "x86_64-linux" then (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.stable.latest.default.override {
-          targets = [ "x86_64-unknown-linux-musl" ];
-        }) else (crane.mkLib pkgs);
+        fenixPkgs = inputs.fenix.packages.${system};
+
+        # TODO: iff I set this up to run on nixos arm don't be so explicit
+        muslToolchain = [ fenixPkgs.targets.x86_64-unknown-linux-musl.stable.rust-std ];
+
+        commonToolchain = [
+          fenixPkgs.stable.cargo
+          fenixPkgs.stable.clippy
+          fenixPkgs.stable.rustc
+          fenixPkgs.stable.rustfmt
+          fenixPkgs.stable.rust-std
+        ];
+
+        toolchain = fenixPkgs.combine (commonToolchain ++ lib.optionals pkgs.stdenv.isLinux muslToolchain);
+
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain toolchain;
 
         src = craneLib.cleanCargoSource ./.;
 
@@ -39,34 +66,51 @@
           inherit src;
           strictDeps = true;
           buildInputs = [ ];
-          nativeBuildInputs = [ ] ++ lib.optionals pkgs.stdenv.isLinux [
-            pkgs.mold-wrapped
-            pkgs.lld
-          ];
+          nativeBuildInputs =
+            [ ]
+            ++ lib.optionals pkgs.stdenv.isLinux [
+              pkgs.mold-wrapped
+              pkgs.lld
+            ];
         };
 
-        staticEnv = {
-          OPENSSL_DIR = "${pkgs.openssl.dev}";
-          OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
-          OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include/";
-        } // lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
-          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-          RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
-        };
+        staticEnv =
+          {
+            OPENSSL_DIR = "${pkgs.openssl.dev}";
+            OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+            OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include/";
+            RUSTFLAGS =
+              "-Aclippy::uninlined_format_args "
+              + lib.optionalString pkgs.stdenv.isLinux "-C link-arg=-fuse-ld=mold ";
+          }
+          // lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
+            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          };
 
-        yeet = craneLib.buildPackage (commonArgs // staticEnv // {
-          CARGO_PROFILE = "dev";
-        });
+        yeet = craneLib.buildPackage (
+          commonArgs
+          // staticEnv
+          // {
+            CARGO_PROFILE = "dev";
+          }
+        );
 
-        yeet-release = craneLib.buildPackage (commonArgs // staticEnv // {
-          CARGO_PROFILE = "release";
-          RUSTFLAGS = "-D warnings";
-        });
+        yeet-release = craneLib.buildPackage (
+          commonArgs
+          // staticEnv
+          // {
+            CARGO_PROFILE = "release";
+            RUSTFLAGS = "-D warnings";
+          }
+        );
       in
       {
+        formatter = treefmtEval.config.build.wrapper;
+
         checks = {
           inherit yeet;
+          formatter = treefmtEval.config.build.check self;
         };
 
         packages = {
@@ -75,18 +119,20 @@
         };
 
         devShells.default = craneLib.devShell {
-          packages = [
-            (pkgs.writeScriptBin "fmtall" ''
-              taplo fmt
-              cargo fmt
-            '')
-            pkgs.cargo-outdated
-            pkgs.cargo-bloat
-            pkgs.cargo-edit
-            pkgs.cargo-unused-features
-            pkgs.gnumake
-            pkgs.taplo
-          ];
+          packages = (
+            with pkgs;
+            [
+              cargo-outdated
+              cargo-bloat
+              cargo-edit
+              cargo-unused-features
+              gnumake
+              taplo
+              treefmt
+              nixfmt-rfc-style
+            ]
+          );
         };
-      });
+      }
+    );
 }
